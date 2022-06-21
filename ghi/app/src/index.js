d08fe1d2a45ea9cb505c0eb7d83dc01e60cab7ae
Shadan <shadanamini@gmail.com>
2022-06-21 14:46:32 -0700

    Changes

--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -8,3 +8,18 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+async function loadSales() {
+  const response = await fetch('http://localhost:8090/api/sales/');
+  if (response.ok) {
+    const data = await response.json();
+    root.render(
+      <React.StrictMode>
+        <App sales={data.sales} />
+      </React.StrictMode>
+    );
+  } else {
+    console.error(response);
+  }
+}
+loadSales();
