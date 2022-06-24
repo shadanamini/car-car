f71cc45163cb8a932c4fbe935055e5872a3860c1
Shadan <shadanamini@gmail.com>
2022-06-24 14:11:12 -0700

    Created Sales Record Form

--- ghi/app/src/SalesRecordForm.js
+++ ghi/app/src/SalesRecordForm.js
@@ -4,19 +4,23 @@ class SalesRecordForm extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+
             automobiles: [],
             salespersons: [],
-            customer: [],
+            potentialcustomers: [],
             salesprice: '',
             automobile: '',
             salesperson: '',
             potentialcustomer: '',
+
         }
 
-        this.handleAutomobileChange = this.handleAutomobileChange.bind(this)
+        this.handleAutomobilesChange = this.handleAutomobilesChange.bind(this)
         this.handleSalesPersonsChange = this.handleSalesPersonsChange.bind(this)
-        this.handleCustomerChange = this.handleCustomerChange.bind(this)
+        this.handleCustomersChange = this.handleCustomersChange.bind(this)
         this.hanldeSalesPriceChange = this.hanldeSalesPriceChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
     }
 
     async handleSubmit(event) {
@@ -24,6 +28,7 @@ class SalesRecordForm extends React.Component {
         const data = {...this.state}
         console.log(data)
         const salesRecordUrl = 'http://localhost:8090/api/sales/';
+        // Confirm whether this is the correct URL
         const fetchConfig = {
             method: "post",
             body: JSON.stringify(data),
@@ -34,60 +39,112 @@ class SalesRecordForm extends React.Component {
         const response = await fetch(salesRecordUrl, fetchConfig)
         if (response.ok) {
             alert('New sales record created!')
-            const newAppointment = await response.json()
-            console.log(newAppointment);
+            const newSalesRecord = await response.json()
+            console.log(newSalesRecord);
             const cleared = {
-                automobiles: '',
-                salespersons: '',
-                customer: '',
+                automobile: '',
+                salesperson: '',
+                potentialcustomer: '',
                 salesprice: '',
             };
+            // Confirm whether these cleared fields are all required to be empty strings
             this.setState(cleared);
         }
     }
 
-    handleAutomobileChange(event) {
+    handleAutomobilesChange(event) {
         const value = event.target.value
-        this.setState({automobiles: value})
+        this.setState({automobile: value})
     }
 
     handleSalesPersonsChange(event) {
         const value = event.target.value
-        this.setState({salespersons: value})
+        this.setState({salesperson: value})
     }
 
-    handleCustomerChange(event) {
+    handleCustomersChange(event) {
         const value = event.target.value
-        this.setState({customer: value})
+        this.setState({potentialcustomer: value})
     }
 
-    async componentDidMount() {
-        const url = 'http://localhost:8090/api/sales_persons/';    
-        const response = await fetch(url);        
-        if (response.ok) {
+    hanldeSalesPriceChange(event) {
+        const value = event.target.value
+        this.setState({salesprice: value})
+    }
+
+
+
+
+
+    // async componentDidMount() {
+    //     try {
+    //       const data = await Promise.all([
+    //         fetch('http://localhost:8090/api/sales/').then((response) => response.json()),
+    //         fetch('http://localhost:8090/api/potential_customers').then((response) => response.json()),
+    //         fetch('http://localhost:8100/api/automobiles').then((response) => response.json())
+    //       ]);
+    //     console.log(data)
+
+
+    //     //   for (var i of data) {
+    //     //     console.log(`RESPONSE ITEM \n`);
+    //     //     for (var obj of i) {
+    //     //       console.log(obj);
+    //     //       //logger utility method, logs output to screen
+    //     //       console.log(obj);
+    //     //     }
+    //     //   }
+    
+    //     } catch (error) {
+    //       console.log(error);
+    //     }
+    //   }
+
+
+    async loadSales() {
+        const response = await fetch("http://localhost:8090/api/sales/");
+        if(response.ok) {
           const data = await response.json();
-          this.setState({salespersons: data.salespersons})
+          console.log(data);
+          this.setState({sales: data.sales});
         }
     }
 
-    async componentDidMount() {
-        const url = 'http://localhost:8090/api/potential_customers/';    
-        const response = await fetch(url);        
-        if (response.ok) {
+    async loadCustomers() {
+        const response = await fetch("http://localhost:8090/api/potential_customers/");
+        if(response.ok) {
           const data = await response.json();
-          this.setState({customer: data.customer})
+          console.log(data);
+          this.setState({potentialcustomers: data.potentialcustomers});
         }
     }
 
-    async componentDidMount() {
-        const url = 'http://localhost:8090/api/potential_customers/';    
-        const response = await fetch(url);        
-        if (response.ok) {
+    async loadAutos() {
+        const response = await fetch("http://localhost:8090/api/automobile_vos/");
+        if(response.ok) {
           const data = await response.json();
-          this.setState({customer: data.customer})
+          console.log(data);
+          this.setState({automobiles: data});
         }
     }
-      
+
+    async loadSalesPersons() {
+        const response = await fetch("http://localhost:8090/api/sales_persons/");
+        if(response.ok) {
+          const data = await response.json();
+          console.log(data);
+          this.setState({salespersons: data.salespersons});
+        }
+    }
+
+
+    async componentDidMount() {
+        this.loadSales();
+        this.loadCustomers();
+        this.loadAutos();
+        this.loadSalesPersons();
+    }
+
 
     render() {
       return (
@@ -96,44 +153,47 @@ class SalesRecordForm extends React.Component {
         <div className="offset-3 col-6">
           <div className="shadow p-4 mt-4">
             <h1>Create a sales record</h1>
-            <form onSubmit={this.handleSubmit} id="create-vehicle-model-form">
+            <form onSubmit={this.handleSubmit} id="create-sales-record-form">
             <div className="mb-3">
-                <select onChange={this.handleAutomobileChange} value={this.state.automobiles} name="automobiles" id="automobiles" className='form-select' required>
+                <select onChange={this.handleAutomobilesChange} value={this.state.automobile} name="automobile" id="automobile" className='form-select' required>
                     <option value="">Choose an automobile</option>
-                    {this.state.automobiles.map(automobiles => {
+                    {this.state.automobiles.map(automobile => {
                         return (
-                            <option key={automobiles.id} value={automobiles.id}>
-                                {automobiles}
+                            <option key={automobile.id} value={automobile.id}>
+                                {automobile.vin}
                             </option>
                         );
                     })}
                 </select>
               </div>
               <div className="mb-3">
-                <select onChange={this.handleSalesPersonsChange} value={this.state.salespersons} name="salespersons" id="salespersons" className='form-select' required>
+                <select onChange={this.handleSalesPersonsChange} value={this.state.salesperson} name="salesperson" id="salesperson" className='form-select' required>
                     <option value="">Choose a sales person</option>
-                    {this.state.salespersons.map(salespersons => {
+                    {this.state.salespersons.map(salesperson => {
                         return (
-                            <option key={salespersons.id} value={salespersons.id}>
-                                {salespersons}
+                            <option key={salesperson.id} value={salesperson.id}>
+                                {salesperson.employee_name}
                             </option>
                         );
                     })}
                 </select>
               </div>
               <div className="mb-3">
-                <select onChange={this.handleCustomerChange} value={this.state.customer} name="customer" id="customer" className='form-select' required>
+                <select onChange={this.handleCustomersChange} value={this.state.potentialcustomer} name="potentialcustomer" id="potentialcustomer" className='form-select' required>
                     <option value="">Choose a customer</option>
-                    {this.state.customer.map(customer => {
+                    {this.state.potentialcustomers.map(potentialcustomer => {
                         return (
-                            <option key={customer.id} value={customer.id}>
-                                {customer}
+                            <option key={potentialcustomer.id} value={potentialcustomer.id}>
+                                {potentialcustomer.customer_name}
                             </option>
                         );
                     })}
                 </select>
               </div>
-              <input type="number" name='Sales price'></input>
+              <div className="form-floating mb-3">
+                <input onChange={this.handleSalesPriceChange} placeholder=" Sales Price" required type="number" name="price" id="price" className="form-control" />
+                <label htmlFor="price">Sales Price</label>
+              </div>
               <button className="btn btn-primary">Create</button>
             </form>
           </div>
