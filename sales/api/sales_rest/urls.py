a2ff3891dfd1fd4f8f0c43acf7c92d96766d67c9
Shadan <shadanamini@gmail.com>
2022-06-24 14:04:13 -0700

    Configured URL Paths

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import api_sales_persons, api_sales_person, api_potential_customers, api_potential_customer, api_sales, api_sale
+from .views import api_automobile_vos, api_sales_persons, api_sales_person, api_potential_customers, api_potential_customer, api_sales, api_sale
 
 urlpatterns = [
     path("sales_persons/", api_sales_persons, name="api_sales_persons"),
@@ -9,4 +9,5 @@ urlpatterns = [
     path("potential_customers/<int:pk>/", api_potential_customer, name="api_potential_customer"),
     path("sales/", api_sales, name="api_sales"),
     path("sales/<int:pk>/", api_sale, name="api_sale"),
+    path("automobile_vos/", api_automobile_vos, name="api_automobile_vos"),
 ]
