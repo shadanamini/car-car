f8e5991f5cc79866c23b10b785dd7cee6bd81690
Shadan <shadanamini@gmail.com>
2022-06-24 14:05:20 -0700

    Registered Models

--- sales/api/sales_rest/admin.py
+++ sales/api/sales_rest/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import SalesPerson, PotentialCustomer
+from .models import SalesPerson, PotentialCustomer, Sales, AutoMobileVO
 
 admin.site.register(SalesPerson)
 admin.site.register(PotentialCustomer)
+admin.site.register(Sales)
+admin.site.register(AutoMobileVO)
